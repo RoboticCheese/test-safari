0ea718c27b4defe3c4202811c062135b30f980e4
Jonathan Hartman <j@p4nt5.com>
2016-05-02 14:11:26 -0700

    Try this

--- test/fixtures/cookbooks/safari_test/recipes/extension.rb
+++ test/fixtures/cookbooks/safari_test/recipes/extension.rb
@@ -4,21 +4,19 @@ end
 
 privacy_services_manager 'allow remote login' do
   service 'accessibility'
-  user node['safari_test']['user']
   applications %w(
-    /System/Library/CoreServices/SystemUIServer.app
-    /System/Library/CoreServices/RemoteManagement/ARDAgent.app
-    /usr/bin/osascript
     com.apple.RemoteDesktopAgent
-    /usr/libexec/sshd-keygen-wrapper
-    com.apple.Safari
   )
-  admin true
 end
+#    /System/Library/CoreServices/SystemUIServer.app
+#    /System/Library/CoreServices/RemoteManagement/ARDAgent.app
+#    /usr/bin/osascript
+#    /usr/libexec/sshd-keygen-wrapper
+#    com.apple.Safari
 
-macosx_gui_login node['safari_test']['user'] do
-  password node['safari_test']['user']
-end
+# macosx_gui_login node['safari_test']['user'] do
+#   password node['safari_test']['user']
+# end
 
 safari_extension 'SafariDriver Extension' do
   safariextz "#{Chef::Config[:file_cache_path]}/SafariDriver.safariextz"
